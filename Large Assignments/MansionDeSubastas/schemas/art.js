629e372fe7c9d4d34c5f8e69f3868360e889ad5a
rakels13 <rakels13@ru.is>
2018-10-10 19:38:15 +0000

    adding schemas

--- Large Assignments/MansionDeSubastas/schemas/art.js
+++ Large Assignments/MansionDeSubastas/schemas/art.js
@@ -1,3 +1,10 @@
 const Schema = require('mongoose').Schema;
 
-module.exports = new Schema();
+module.exports = new Schema({
+  title: { type: String, required: true },
+  artistId: {type: Schema.Types.ObjectId, required: true },
+  date: { type: Date, default: Date.now, required: true},
+  images: { type: [String], required: true },
+  description: { type: String},
+  isAuctionItem: { type: Boolean, default: false }
+});
